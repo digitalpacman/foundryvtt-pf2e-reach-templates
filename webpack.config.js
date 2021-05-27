67067f9e757a30f2bc90acbaf8c68cca4e3c2d7d
Brad Laney <brad.j.laney@gmail.com>
2021-05-27 16:44:25 -0500

    move module.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,4 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: './src/main.js',
   mode: 'production',
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: 'src/module.json', to: '' }
-      ]
-    })
-  ]
 };
