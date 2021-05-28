f19384c30b0005e4a05ff7a270d81c4b0a1eaed9
Brad Laney <brad.j.laney@gmail.com>
2021-05-27 21:38:48 -0500

    remove dist. add package as zip. add version file

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,11 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: './src/main.js',
   mode: 'production',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "module.json", to: "" },
+      ],
+    }),
+  ],
 };
