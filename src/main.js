d32754558147062d16ff14df877ad9d0759dd6b6
Brad Laney <brad.j.laney@gmail.com>
2021-06-17 02:47:01 -0500

    add hotkey config setting

--- src/main.js
+++ src/main.js
@@ -7,6 +7,42 @@ const Mutex = require('async-mutex').Mutex;
 Hooks.on("ready", function() {
   document.addEventListener('keydown', handleKeyDown);
 
+  game.settings.register('pf2e-reach-templates', 'hotkey', {
+    name: `Hotkey`,
+    scope: 'world',
+    config: true,
+    default: 'r',
+    type: String,
+    choices: {
+      "a": "a",
+      "b": "b",
+      "c": "c",
+      "d": "d",
+      "e": "e",
+      "f": "f",
+      "g": "g",
+      "h": "h",
+      "i": "i",
+      "j": "j",
+      "k": "k",
+      "l": "l",
+      "m": "m",
+      "n": "n",
+      "o": "o",
+      "p": "p",
+      "q": "q",
+      "r": "r",
+      "s": "s",
+      "t": "t",
+      "u": "u",
+      "v": "v",
+      "w": "w",
+      "x": "x",
+      "y": "y",
+      "z": "z",
+    },
+  });
+
   for (let i = 0; i < defaultColors.length; ++i) {
     const color = defaultColors[i];
     game.settings.register('pf2e-reach-templates', `color_${color}`, {
@@ -15,7 +51,7 @@ Hooks.on("ready", function() {
       config: true,
       default: color,
       type: String,
-    });
+    });  
   }
 
   resetColors();
@@ -25,7 +61,7 @@ const mutex = new Mutex();
 
 async function handleKeyDown(event) {
   const token = TokenLayer.instance.controlled[0];
-  if (!token || event.key !== 'r') return;
+  if (!token || event.key !== game.settings.get('pf2e-reach-templates', 'hotkey')) return;
   if (document.body !== document.activeElement) return;
 
   if (mutex.isLocked()) return;
